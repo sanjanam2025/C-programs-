b7815d2c748cb49bdf948f9ba4e1b1033bc3b171
Sanjana.M <sanjana.m189@gmail.com>
2025-11-10 18:22:57 +0530

    Add files via upload

---
+++ Prgm 17.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    int age;
+    float height;
+    char grade;
+    printf("enter age:");
+    scanf(" %d",&age);
+    printf("enter height:");
+    scanf(" %f",&height);
+    printf("enter grade(A/B/C):");
+    scanf(" %c",&grade);
+    printf("\n---student details---\n");
+    printf("age: %d\n",age);
+    printf("height: %.1f\n",height);
+    printf("grade:%c\n",grade);
+    return 0;
+}
---
+++ Prgm 22.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+int a=5,b=10;
+printf("result1 :%d\n",(a<b)&&(b>0));
+printf("result2 :%d\n",(a>b)||(b>0));
+printf("result3 :%d\n",!(a==b));
+return 0;
+}
