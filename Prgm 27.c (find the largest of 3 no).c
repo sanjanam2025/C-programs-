f84f6b9836daced365ab87f3e462886079ee7706
Sanjana.M <sanjana.m189@gmail.com>
2025-11-11 18:43:29 +0530

    Add files via upload

---
+++ Prgm 27.c (find the largest of 3 no).c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+int a,b,c;
+printf("enter three numbers:");
+scanf("%d %d %d",&a,&b,&c);
+if(a>b&&a>c)
+{
+printf("largest number=%d\n",a);
+}
+else if (b>a&&b>c)
+{
+printf("largest number=%d\n",b);
+}
+else
+{
+printf("largest number=%d\n",c);
+}
+return 0;
+}
---
+++ Prgm 28.c (check positivity of 2 nos using nested if).c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+int a,b;
+printf("enter two numbers:");
+scanf("%d %d",&a,&b);
+if(a>0)
+{
+if(b>0)
+{
+printf("both numbers are positive\n");
+}
+}
+return 0;
+}
